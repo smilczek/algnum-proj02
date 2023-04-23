3f29efce70ff9f303ece7bd1e85edf01ca30f6b1
smilczek <s.milczek.670@studms.ug.edu.pl>
2023-04-24 00:21:06 +0200

    fixed yo meme

--- uklady-rownan-liniowych/src/Matrix/MySparseMatrix.java
+++ uklady-rownan-liniowych/src/Matrix/MySparseMatrix.java
@@ -216,6 +216,8 @@ public abstract class MySparseMatrix {
 
             // Subtract current row from the rows below it
             for (int row = currRow + 1; row < this.getNumRows(); row++) {
+                if (this.getElement(row, column) == 0)
+                    continue;
                 this.subtractRowsScalar(row, currRow, this.getElement(row, column) / firstElem);
             }
             currRow++;
@@ -244,6 +246,8 @@ public abstract class MySparseMatrix {
 
             // Subtract current row from the rows below it
             for (int row = currRow + 1; row < this.getNumRows(); row++) {
+                if (this.getElement(row, column) == 0)
+                    continue;
                 this.subtractRowsScalar(row, currRow, this.getElement(row, column) / firstElem);
             }
             currRow++;
