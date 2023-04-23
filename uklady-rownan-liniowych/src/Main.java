b8d3568b1a418844bda9bcc67ff1f350f5bfae8b
Zuzanna Urbanska <zuz.urbanska@wp.pl>
2023-04-23 19:39:53 +0200

    fix main??

--- uklady-rownan-liniowych/src/Main.java
+++ uklady-rownan-liniowych/src/Main.java
@@ -2,24 +2,8 @@ import Matrix.MySparseMatrixArray;
 import Matrix.MySparseMatrixHashMap;
 
 public class Main {
-    public static void main(String[] args) throws java.io.IOException {
-//        MySparseMatrixArray mat = new MySparseMatrixArray(3, 3);
-//        mat.generateSparse();
-//        mat.saveToFile("matrix.txt");
+    public static void main(String[] args) {
 
-//        MySparseMatrixHashMap mat1 = new MySparseMatrixHashMap("matrix.txt");
-//        MySparseMatrixArray mat2 = new MySparseMatrixArray("matrix.txt");
-//        mat1.printMatrix();
-//        mat2.printMatrix();
-
-//        mat1.solveA1();
-//        mat2.solveA2();
-
-//        mat1.printSolved();
-//        mat2.printSolved();
-
-        EfficiencyTests test = new EfficiencyTests();
-        test.testSparseArray(50);
 
     }
 }
