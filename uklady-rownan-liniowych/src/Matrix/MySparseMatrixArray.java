3f35be0a91a8d888a153ed849bd47dfeed49a42b
awawron <a.wawron.693@studms.ug.edu.pl>
2023-04-23 16:02:31 +0200

    New constructors from given matrix

--- uklady-rownan-liniowych/src/Matrix/MySparseMatrixArray.java
+++ uklady-rownan-liniowych/src/Matrix/MySparseMatrixArray.java
@@ -2,9 +2,30 @@ package Matrix;
 public class MySparseMatrixArray extends MySparseMatrix {
     private double[][] nonZeroElements;
 
+    // constructor for empty matrix
     public MySparseMatrixArray(int numRows, int numCols) {
         super(numRows, numCols);
         this.nonZeroElements = new double[numRows][numCols];
+    }
+
+    // constructor for given matrix as a 2d array
+    public MySparseMatrixArray(double[][] matrix) {
+        // found out super() needs to be the first thing in the constructor,
+        // so it has no names for the arguments
+        // it translates to super(numRows, numCols)
+        super(matrix.length, matrix[0].length);
+        int numRows = matrix.length;
+        int numCols = matrix[0].length;
+
+        this.nonZeroElements = new double[numRows][numCols];
+
+        for (int row = 0; row < numRows; row++) {
+            for (int col = 0; col < numCols; col++) {
+                if(matrix[row][col] != 0) {
+                    this.setElement(row, col, matrix[row][col]);
+                }
+            }
+        }
     }
 
     @Override
--- uklady-rownan-liniowych/src/Matrix/MySparseMatrixHashMap.java
+++ uklady-rownan-liniowych/src/Matrix/MySparseMatrixHashMap.java
@@ -5,9 +5,30 @@ import java.util.HashMap;
 public class MySparseMatrixHashMap extends MySparseMatrix {
     private HashMap<String, Double> nonZeroElements;
 
+    // constructor for empty matrix
     public MySparseMatrixHashMap(int numRows, int numCols) {
         super(numRows, numCols);
         this.nonZeroElements = new HashMap<>();
+    }
+
+    // constructor for given matrix as a 2d array
+    public MySparseMatrixHashMap(double[][] matrix) {
+        // just like in MySparseMatrixArray super() needs to be
+        // the first thing in the constructor, so it has no names for the arguments
+        // it translates to super(numRows, numCols)
+        super(matrix.length, matrix[0].length);
+        int numRows = matrix.length;
+        int numCols = matrix[0].length;
+
+        this.nonZeroElements = new HashMap<>();
+
+        for (int row = 0; row < numRows; row++) {
+            for (int col = 0; col < numCols; col++) {
+                if(matrix[row][col] != 0) {
+                    this.setElement(row, col, matrix[row][col]);
+                }
+            }
+        }
     }
 
     @Override
