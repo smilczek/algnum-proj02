edaef41a0a26dea7ea422bd9829545efe3bb52cd
awawron <a.wawron.693@studms.ug.edu.pl>
2023-04-23 19:58:56 +0200

    CorrectnessTests declared

---
+++ uklady-rownan-liniowych/src/Tests/CorrectnessTests.java
@@ -0,0 +1,25 @@
+package Tests;
+
+public class CorrectnessTests {
+    private double[] TG_results;
+    private double[] TW_results;
+    private double[] TR_results;
+
+    public void TG_test() {
+        for (int size = 10; size < 100; size++) {
+            // define and generate matrices
+            // do the test
+            // write down results
+        }
+
+        // save results?
+    }
+
+    public void TW_test() {
+
+    }
+
+    public void TR_test() {
+
+    }
+}
