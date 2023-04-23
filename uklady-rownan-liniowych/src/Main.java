e89fcaf975c5a7f4422cf33b909330061b1aca3b
awawron <a.wawron.693@studms.ug.edu.pl>
2023-04-23 16:57:09 +0200

    Example usage of saving and loading matrices

--- uklady-rownan-liniowych/src/Main.java
+++ uklady-rownan-liniowych/src/Main.java
@@ -1,10 +1,20 @@
 import Matrix.MySparseMatrixArray;
 import Matrix.MySparseMatrixHashMap;
 
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
-        MySparseMatrixArray mat = new MySparseMatrixArray(3, 3);
-        mat.generateSparse();
-        mat.solve();
+    public static void main(String[] args) throws IOException {
+//        MySparseMatrixArray mat = new MySparseMatrixArray(3, 3);
+//        mat.generateSparse();
+//        mat.solve();
+
+        double[][] testarr = new double[][]{{1, 2, 0}, {3, 0, 1}, {0, 0, 5}};
+        MySparseMatrixHashMap testmatrix = new MySparseMatrixHashMap(testarr);
+        testmatrix.saveToFile("file");
+
+        MySparseMatrixArray testmatrix2 = new MySparseMatrixArray("file");
+
+        testmatrix2.printMatrix();
     }
 }
